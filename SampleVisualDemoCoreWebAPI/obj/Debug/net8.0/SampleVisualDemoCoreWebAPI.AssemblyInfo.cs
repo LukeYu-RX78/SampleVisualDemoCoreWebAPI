8b455ae6097c1459c58cd61f4479429a40f1147c
luke.yu <yangyi.yu.luke@gmail.com>
2025-05-07 12:02:11 +1000

    publish in vs code

--- SampleVisualDemoCoreWebAPI/obj/Debug/net8.0/SampleVisualDemoCoreWebAPI.AssemblyInfo.cs
+++ SampleVisualDemoCoreWebAPI/obj/Debug/net8.0/SampleVisualDemoCoreWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleVisualDemoCoreWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0925a734af1db0d84c91e1ad493e2454d4a28f70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aba6fd91d955b935d19aff6638977939e85b975")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleVisualDemoCoreWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleVisualDemoCoreWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
